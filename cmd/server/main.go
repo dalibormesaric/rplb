3ddaab1986fafed4cfb86b9791d4ef88533b4240
Dalibor Mesarić <dalibormesaric@users.noreply.github.com>
2024-07-07 14:05:11 +0200

    Refactor dashboard from main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"embed"
 	"flag"
-	"html/template"
 	"log"
 	"net/http"
 
@@ -13,12 +11,6 @@ import (
 	"github.com/dalibormesaric/rplb/internal/reverseproxy"
 	"github.com/dalibormesaric/rplb/internal/server"
 )
-
-//go:embed template/*.html
-var content embed.FS
-
-//go:embed assets/*.css assets/*.js
-var assets embed.FS
 
 var (
 	fe = flag.String("f", "", "frontends")
@@ -50,29 +42,5 @@ func main() {
 	http.HandleFunc("/ws", wsServer.WsHandler)
 	go wsServer.Broadcaster()
 
-	http.Handle("/assets/", http.StripPrefix("/", http.FileServer(http.FS(assets))))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/monitor", http.StatusPermanentRedirect)
-	})
-
-	http.HandleFunc("/monitor", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		a, _ := template.
-			New("index").
-			Funcs(dashboard.GetFuncMap()).
-			ParseFS(content, "template/index.html", "template/monitor.html")
-		a.ExecuteTemplate(w, "monitor.html", dashboard.MonitorModel{BaseModel: &dashboard.BaseModel{SelectedMenu: "monitor"}, Backends: backends})
-	})
-
-	http.HandleFunc("/traffic", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		a, _ := template.
-			New("index").
-			Funcs(dashboard.GetFuncMap()).
-			ParseFS(content, "template/index.html", "template/traffic.html")
-		a.ExecuteTemplate(w, "traffic.html", dashboard.TrafficModel{BaseModel: &dashboard.BaseModel{SelectedMenu: "traffic"}, Frontends: frontends, Backends: backends})
-	})
-
-	http.ListenAndServe(":8000", nil)
+	dashboard.ListenAndServe(frontends, backends)
 }
--- internal/dashboard/dashboard.go
+++ internal/dashboard/dashboard.go
@@ -1,15 +1,54 @@
 package dashboard
 
 import (
+	"embed"
 	"html/template"
+	"net/http"
 	"time"
+
+	"github.com/dalibormesaric/rplb/internal/backend"
+	"github.com/dalibormesaric/rplb/internal/frontend"
 )
+
+//go:embed template/*.html
+var content embed.FS
+
+//go:embed assets/*.css assets/*.js
+var assets embed.FS
 
 var (
 	since = time.Now()
 )
 
-func GetFuncMap() template.FuncMap {
+func ListenAndServe(frontends frontend.Frontends, backends backend.Backends) {
+	http.Handle("/assets/", http.StripPrefix("/", http.FileServer(http.FS(assets))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/monitor", http.StatusPermanentRedirect)
+	})
+
+	http.HandleFunc("/monitor", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		a, _ := template.
+			New("index").
+			Funcs(getFuncMap()).
+			ParseFS(content, "template/index.html", "template/monitor.html")
+		a.ExecuteTemplate(w, "monitor.html", MonitorModel{BaseModel: &BaseModel{SelectedMenu: "monitor"}, Backends: backends})
+	})
+
+	http.HandleFunc("/traffic", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		a, _ := template.
+			New("index").
+			Funcs(getFuncMap()).
+			ParseFS(content, "template/index.html", "template/traffic.html")
+		a.ExecuteTemplate(w, "traffic.html", TrafficModel{BaseModel: &BaseModel{SelectedMenu: "traffic"}, Frontends: frontends, Backends: backends})
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
+
+func getFuncMap() template.FuncMap {
 	return template.FuncMap{
 		"printSince": func() string {
 			return since.Format("2006-01-02 15:04:05")
