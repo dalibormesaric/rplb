1d7bad53cda5038a579b30ece78f08990a08c81d
Dalibor Mesarić <dalibormesaric@users.noreply.github.com>
2024-09-09 06:45:24 +0200

    Move dashboard models

--- internal/dashboard/dashboard.go
+++ internal/dashboard/dashboard.go
@@ -11,6 +11,22 @@ import (
 	"github.com/dalibormesaric/rplb/internal/frontend"
 )
 
+type BaseModel struct {
+	SelectedMenu string
+	Version      string
+}
+
+type MonitorModel struct {
+	BaseModel
+	Backends backend.BackendPool
+}
+
+type TrafficModel struct {
+	BaseModel
+	Frontends frontend.Frontends
+	Backends  backend.BackendPool
+}
+
 //go:embed templates/*.html
 var content embed.FS
 
