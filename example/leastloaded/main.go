069029e7a358ba46f616aff68af7cd3748622750
Dalibor Mesarić <dalibormesaric@users.noreply.github.com>
2024-09-22 17:48:19 +0200

    Adjust least loaded example

--- example/leastloaded/main.go
+++ example/leastloaded/main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-var delay = flag.Int("delay", 1000, "Delay in milliseconds.")
+var delay = flag.Int("delay", 0, "Delay in milliseconds.")
 
 func main() {
 	flag.Parse()
@@ -16,9 +16,13 @@ func main() {
 	log.Println("Starting delay server on 8888")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		d := time.Duration(*delay * int(time.Millisecond))
-		time.Sleep(d)
-		fmt.Fprintf(w, "Response after %s milliseconds.\n", d)
+		if *delay > 0 {
+			d := time.Duration(*delay * int(time.Millisecond))
+			time.Sleep(d)
+			fmt.Fprintf(w, "Response after %s milliseconds.\n", d)
+		} else {
+			fmt.Fprintf(w, "Response.\n")
+		}
 	})
 	http.ListenAndServe(":8888", nil)
 }
