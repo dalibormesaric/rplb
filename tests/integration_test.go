a1bcb4b91b6593ef5a7802e4b3944be0fd96e09a
Dalibor Mesarić <dalibormesaric@users.noreply.github.com>
2024-10-09 16:00:49 +0200

    Refactor integration tests

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -2,7 +2,9 @@ package integration
 
 import (
 	"bufio"
+	"log"
 	"net/http"
+	"os/exec"
 	"slices"
 	"strings"
 	"sync"
@@ -116,5 +118,28 @@ func TestIntegration(t *testing.T) {
 		}
 
 		tearDown()
+	}
+}
+
+func setUp(algo string) {
+	out, err := exec.Command("docker", "-v").Output()
+	if err != nil {
+		log.Fatalf("Docker not running: (%v)", err)
+	}
+	if !strings.HasPrefix(string(out[:]), "Docker version") {
+		log.Fatal("Docker not running")
+	}
+
+	// TODO: check if env file exists
+	err = exec.Command("docker", "compose", "--env-file", "."+algo+".env", "-f", "../example/compose.yaml", "up", "-d", "rplb", "--build").Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func tearDown() {
+	err := exec.Command("docker", "compose", "-f", "../example/compose.yaml", "down").Run()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
